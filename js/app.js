f6c17aa735da270622b477b79e9f0c3b5be13eb6
Andrew Wong <andrew.wongsf@gmail.com>
2016-08-13 08:50:30 -0400

    filter search by year of release

--- js/app.js
+++ js/app.js
@@ -1,30 +1,50 @@
 $('form').submit(function (evt) {
+	
 	evt.preventDefault();
+
 	var omdbAPI = "http://www.omdbapi.com/?";
 	var searchValue = $("#search").val();
+	var year = $("#year").val();
 	var omdbOptions = {
-		s: searchValue
+		s: searchValue,
+		y: year
 	};
+
 	function displayMovies(data) {
+
 		console.log(data);
+
 		var moviesHTML = '';
+
 		if (data.Response === "True") {
+
 			$.each(data.Search, function (i, movie) {
+
 				moviesHTML += '<li><div class="poster-wrap">';
+
 				if (movie.Poster === "N/A") {
+
 					moviesHTML += '<i class="material-icons poster-placeholder">crop_original</i>';
+
 				} else {
+
 					moviesHTML += '<img class="movie-poster" src="' + movie.Poster + '"></div>';	
+
 				}
+
 				moviesHTML += '<span class="movie-title">' + movie.Title + '</span>';
 				moviesHTML += '<span class="movie-year">' + movie.Year + '</span></li>';
 			});
+
 		} else if (data.Response === "False") {
+
 			moviesHTML += '<li class="no-movies">';
 			moviesHTML += '<i class="material-icons icon-help">help_outline</i>No movies found that match: ' + searchValue + '.';
 			moviesHTML += '</li>';
 		}
+
 		$("#movies").html(moviesHTML);
 	}
+
 	$.getJSON(omdbAPI, omdbOptions, displayMovies);
 });
