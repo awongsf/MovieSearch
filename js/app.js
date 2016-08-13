fd69fc3bad3b41ba5a4cebcb7db0d6674e4a4dfd
Andrew Wong <andrew.wongsf@gmail.com>
2016-08-12 21:23:58 -0400

    display placeholder icon for N/A posters, display text for empty search results

--- js/app.js
+++ js/app.js
@@ -8,12 +8,22 @@ $('form').submit(function (evt) {
 	function displayMovies(data) {
 		console.log(data);
 		var moviesHTML = '';
-		$.each(data.Search, function (i, movie) {
-			moviesHTML += '<li><div class="poster-wrap">';
-			moviesHTML += '<img class="movie-poster" src="' + movie.Poster + '"></div>';
-			moviesHTML += '<span class="movie-title">' + movie.Title + '</span>';
-			moviesHTML += '<span class="movie-year">' + movie.Year + '</span></li>';
-		});
+		if (data.Response === "True") {
+			$.each(data.Search, function (i, movie) {
+				moviesHTML += '<li><div class="poster-wrap">';
+				if (movie.Poster === "N/A") {
+					moviesHTML += '<i class="material-icons poster-placeholder">crop_original</i>';
+				} else {
+					moviesHTML += '<img class="movie-poster" src="' + movie.Poster + '"></div>';	
+				}
+				moviesHTML += '<span class="movie-title">' + movie.Title + '</span>';
+				moviesHTML += '<span class="movie-year">' + movie.Year + '</span></li>';
+			});
+		} else if (data.Response === "False") {
+			moviesHTML += '<li class="no-movies">';
+			moviesHTML += '<i class="material-icons icon-help">help_outline</i>No movies found that match: ' + searchValue + '.';
+			moviesHTML += '</li>';
+		}
 		$("#movies").html(moviesHTML);
 	}
 	$.getJSON(omdbAPI, omdbOptions, displayMovies);
